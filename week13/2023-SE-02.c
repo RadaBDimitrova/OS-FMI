ae08a312ece22718d72c1a1dcc9caa5499f6a76c
Rada Dimitrova <117522044+RadaBDimitrova@users.noreply.github.com>
2026-05-19 11:58:24 +0300

    Update 2023-SE-02.c

--- week13/2023-SE-02.c
+++ week13/2023-SE-02.c
@@ -1,65 +1,77 @@
 #include <unistd.h>
 #include <err.h>
-#include <fcntl.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
-#include <stdio.h>
 
 const char* str = "found it!";
 
 int main(int argc, char* argv[]) {
+	
+    if (argc < 2) {
+        errx(26, "at least 1 arg needed");
+    }
 
-	if (argc < 2) {
-		errx(26, "at least 1 arg needed");
-	}
-
-	int pipefd[2];
+    int pipefd[2];
     if (pipe(pipefd) < 0) {
-		err(26, "err during pipe");
-	}
+        err(26, "err during pipe");
+    }
 
+    int children[argc - 1];
 
-	for (int i = 1; i < argc; i++) {
-		int proc = fork();
-		if (proc < 0) {
-			err(26, "err forking");
-		}
+    for (int i = 1; i < argc; i++) {
+        int pid = fork();
 
-		if (proc == 0) {
-			close(pipefd[0]);
+        if (pid < 0) {
+            err(26, "err forking");
+        }
+
+        if (pid == 0) {
+            close(pipefd[0]);
             if (dup2(pipefd[1], 1) < 0) {
                 err(26, "err during dup2");
             }
-			close(pipefd[1]);
-            execlp(argv[i], argv[i], (const char*) NULL);
+
+            close(pipefd[1]);
+            execlp(argv[i], argv[i], (char*)NULL);
             err(26, "child failed to execute process %s", argv[i]);
-		}
+        }
+        children[i - 1] = pid;
+    }
 
-	}
+    close(pipefd[1]);
 
-	close(pipefd[1]);
+    char byte;
+    size_t curr = 0;
+    while (read(pipefd[0], &byte, 1) == 1) {
+        if (byte == str[curr]) {
+            curr++;
+        } else {
+            if (byte == str[0]) {
+    			curr = 1;
+			} else {
+    			curr = 0;
+			}
+        }
 
-	char byte;
-	size_t curr = 0;
-	while (read(pipefd[0], &byte, sizeof(char)) == sizeof(char)) {
-		if (byte == str[curr]) {
-			curr++;
-		} else {
-			curr = 0;	
-		}
-
-		if (curr == strlen(str)) {
-			close(pipefd[0]);
-            if (kill(0, SIGTERM) < 0) { //kills all processes in the group
-                err(26, "failed to kill all children");
+        if (curr == strlen(str)) {
+            for (int i = 0; i < argc - 1; i++) {
+                kill(children[i], SIGTERM);
             }
-			exit(0);
-		}
+            for (int i = 0; i < argc - 1; i++) {
+                waitpid(children[i], NULL, 0);
+            }
+            close(pipefd[0]);
+            exit(0);
+        }
+    }
 
-	}
-	close(pipefd[0]);
+    close(pipefd[0]);
 
-	exit(1);
+    for (int i = 0; i < argc - 1; i++) {
+        waitpid(children[i], NULL, 0);
+    }
+
+    exit(1);
 }
